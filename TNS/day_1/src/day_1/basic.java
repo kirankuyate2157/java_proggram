aceb7a23e63579fc727d02cfa135889fd0f9ab73
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-10-30 21:25:08 +0530

    day 1 done

---
+++ TNS/day_1/src/day_1/basic.java
@@ -0,0 +1,79 @@
+package day_1;
+
+public class basic {
+
+	public static void main(String[] args) {
+
+		// char a = 'a';
+		// System.out.println(a);
+		// char a1 = 67;
+		// System.out.println(a1);
+		// char a2 = '\u00A7';
+		// System.out.println(a2);
+		// char a3 = '\u20AC';
+		// System.out.println(a3);
+		// int k = 'A';
+		// System.out.println(k);
+
+		// numbers
+		// int num = 77;
+		// int val = 9 / 2;
+		// float val2 = 36552242f / 2f;
+		// double val3 = 36552242d / 2d;
+		// float val4 = 5 / 2.5f;
+		// System.out.println("num :" + num);
+		// System.out.println("value :" + val);
+		// System.out.println("val2 :" + val2);
+		// System.out.println("val3 :" + val3);
+		// System.out.println("val4 :" + val4);
+
+		// int marker = 512;
+		// double percentage = marker * 0.46f + 56;
+		// System.out.println("percentage : " + percentage);
+
+		// valid identifiers
+		// .......
+
+		// primitive Data types
+
+		// byte takes - 1 byte it takes 4 bits or 8 bits depending on systems
+		// byte byteMax = 127;
+		// byte byteMin = -128;
+		// System.out.println("Min range of bytes is " + byteMin + " Max range of bytes
+		// is " + byteMax);
+
+		// // short - 2 bytes 10^5
+		// short shortMax = 32767;
+		// short shortMin = -32767;
+		// System.out.println("Min range of short is " + shortMin + " Max range of short
+		// is " + shortMax);
+
+		// // int - 4 bytes 10^9
+		// int IntMax = 2147483647;
+		// int IntMin = -2147483648;
+		// System.out.println("min range of int is " + IntMin + " Max range of int is "
+		// + IntMax);
+
+		// // long -8 bytes 10^18
+		// long longMax = 9223372036854775807L;
+		// long longMin = -9223372036854775808L;
+		// System.out.println("min range of long is " + longMin + " Max range of long "
+		// + longMax);
+
+		// // boolean 1/0
+		// boolean flg = false;
+		// System.out.println("the boolean flag value is " + flg);
+
+		// float f = 3234.141243278345f;
+		// double d = 3234.14124512345678902345678914f;
+		// System.out.println("float value is " + f + " double value is " + d);
+
+		// narrowing//Explicit type casting
+		double d1 = 68957937579.777d;
+		long l2 = (long) d1;
+		float f3 = (float) d1;
+		System.out.println(l2);
+		System.out.println(f3);
+	}
+
+}
